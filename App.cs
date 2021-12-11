236149b3767550d87b89674b357ad8472ab210bd
bigbali <bigbali141@gmail.com>
2021-12-11 22:35:30 +0200

    Initial

---
+++ App.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace simple_ytdl_cs
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {}
+    public static class Globals
+    {
+        public static string url = string.Empty;
+        public static string folder = string.Empty;
+    }
+}
---
+++ Handlers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+
+namespace simple_ytdl_cs
+{
+    public partial class SYTDLWindow : Window
+    {
+        //private string sURL = string.Empty;
+        private void toggleUrlLock(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private async void downloadSingle(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(Globals.url);
+            //Globals.url = urlInputField.Text;
+            //sURL = sUrl.Text;
+            //MessageBox.Show(sURL);
+
+            ProcessStartInfo process = new ProcessStartInfo("./youtube-dl");
+            process.Arguments = "--help";
+            process.CreateNoWindow = true;
+
+            using (Process ytdl = Process.Start(process))
+            {
+                Task exit = ytdl.WaitForExitAsync();
+                await exit;
+                MessageBox.Show(exit.IsCompleted.ToString());
+                MessageBox.Show(exit.IsCompletedSuccessfully.ToString());
+            }
+        }
+        private void downloadPlaylist(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void toggleFolderLock(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void selectFolder(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
